811b43c86e11cb517a0b2e0852f3eb0063c05c7a
Sergei Gavrilov <sergei.k.gavrilov@gmail.com>
2023-03-08 11:50:19 +0500

    spring project starter connecttion to postgress (file .properties)

---
+++ src/main/java/com/jit/rec/recipetoria/RecipetoriaApplication.java
@@ -0,0 +1,13 @@
+package com.jit.rec.recipetoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipetoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipetoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jit/rec/book/recipetoria/RecipetoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jit.rec.book.recipetoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipetoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
