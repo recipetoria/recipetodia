d84efb57e0416c70a63db767f525551c6dbeca30
Nikolai Khriapov <kolyakhryapov@gmail.com>
2023-04-10 22:29:14 +0800

    1. CORS issue

--- src/main/java/com/jit/rec/recipetoria/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/jit/rec/recipetoria/security/configuration/SecurityConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -39,4 +41,20 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("Authorization");
+            }
+        };
+    }
+
 }
