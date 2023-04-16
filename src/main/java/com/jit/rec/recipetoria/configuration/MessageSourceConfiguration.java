f206ba584dd83c3040ce5632902f9534d5b86fc4
Nikolai Khriapov <kolyakhryapov@gmail.com>
2023-04-16 22:25:45 +0800

    1. Handled few more exceptions; 2. Put validation messages in a .properties file; 3. Created a separate package for enumerations

---
+++ src/main/java/com/jit/rec/recipetoria/configuration/MessageSourceConfiguration.java
@@ -0,0 +1,18 @@
+package com.jit.rec.recipetoria.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:validation");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
